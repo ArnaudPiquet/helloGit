370dc661436469ab2ff2c1e6234aaa25760e59c1
ArnaudPiquet <arnaud.piquet@gmail.com>
2018-02-06 15:45:57 +0100

    Ajout WS Horloge + JQUERY

--- ProjetRest/src/main/java/fr/m2i/formation/rest/ws/PersonneWS.java
+++ ProjetRest/src/main/java/fr/m2i/formation/rest/ws/PersonneWS.java
@@ -1,5 +1,8 @@
 package fr.m2i.formation.rest.ws;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,7 +15,6 @@ import fr.m2i.formation.rest.bean.Personne;
 public class PersonneWS {
 
 	@GET
-	@Path("/text")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getPersonneText() {
 		Personne p = new Personne("Dupond","Jules", new Adresse("rue Lajoie", "44000", "Nantes", 15));
@@ -20,17 +22,41 @@ public class PersonneWS {
 	}
 	
 	@GET
-	@Path("/xml")
 	@Produces(MediaType.APPLICATION_XML)
 	public Personne getPersonneXml() {
 		Personne p = new Personne("Lavoisier","Anne", new Adresse("rue Colbert", "37000", "Tours", 5));
 		return p;
 	}
 	@GET
-	@Path("/json")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Personne getPersonneJson() {
 		Personne p = new Personne("Lavoisier","Anne", new Adresse("rue Colbert", "37000", "Tours", 5));
 		return p;
 	}
+	
+	
+	@GET
+	@Path("/listexml")
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Personne> getListePersonneTxt() {
+		Personne p1 = new Personne("Lavoisier","Anne", new Adresse("rue Colbert", "37000", "Tours", 5));
+		Personne p2 = new Personne("Dupond","Jules", new Adresse("rue Lajoie", "44000", "Nantes", 15));
+		List<Personne> listeP = new ArrayList<Personne>();
+		listeP.add(p1);
+		listeP.add(p2);
+		return listeP;
+	}
+	
+	@GET
+	@Path("/listejson")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Personne> getListePersonneJson() {
+		Personne p1 = new Personne("Lavoisier","Anne", new Adresse("rue Colbert", "37000", "Tours", 5));
+		Personne p2 = new Personne("Dupond","Jules", new Adresse("rue Lajoie", "44000", "Nantes", 15));
+		List<Personne> listeP = new ArrayList<Personne>();
+		listeP.add(p1);
+		listeP.add(p2);
+		return listeP;
+	}
+	
 }
